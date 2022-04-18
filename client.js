9645589c87717dbec2a529c342bd715bb4029bc7
ysave <nick.c@menisch.ch>
2022-04-18 19:02:44 +0200

    Sending message to the Server

--- client.js
+++ client.js
@@ -1,5 +1,15 @@
 const socket = io('http://localhost:3000')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
 
 socket.on('chat-message', data => {
     console.log(data)
+})
+
+//Sends a Message to the Server
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    socket.emit('send-message', message)
+    messageInput.value = ''
 })
